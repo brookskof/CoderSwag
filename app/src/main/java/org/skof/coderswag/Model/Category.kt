ffe5b22650e82c1a182bbec912ef7d059cd407b2
brookskof <brook@skof.org>
2017-11-19 15:38:36 -0800

    lesson 02

--- app/src/main/java/org/skof/coderswag/Model/Category.kt
+++ app/src/main/java/org/skof/coderswag/Model/Category.kt
@@ -3,4 +3,8 @@ package org.skof.coderswag.Model
 /**
  * Created by regis on 11/19/2017.
  */
-class Category(val title: String, val image: String)
+class Category(val title: String, val image: String) {
+    override fun toString(): String {
+        return title
+    }
+}
